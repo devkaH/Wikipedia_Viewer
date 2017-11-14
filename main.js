a5e5af02c970d1b8e4e7433061a607746a7ff9ee
kaoula <ben.hammadi.kaoula@gmail.com>
2017-11-14 12:02:35 +0100

    update content

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var apiURL = "https://en.wikipedia.org/w/api.php?callback=?";
 
-
-$('#searchSubmit').click(function() {
+console.log("hello")
+function click() {
 
   $('#container').append("<img id='clearIcon' src='icon.png'>") ;
   // clear prior search results 
@@ -9,7 +9,7 @@ $('#searchSubmit').click(function() {
    $("#searchResult").empty();
    $("#search").val('');
 
- })
+ });
 
 
     // clear prior search results 
@@ -18,37 +18,50 @@ $('#searchSubmit').click(function() {
       format: 'json',
       inprop: "url",
       formatversion: 2,
-      prop: 'extracts|pageimages',
+      prop: 'extracts|pageimages|info',
       generator: 'search',
       gsrsearch: $("#search").val(),
       gsrwhat: "text",
-      prop: 'extracts|info',
+      piprop: 'thumbnail',
+      pilimit: 'max',
+      pithumbsize: 200,
       exsentences: 3,
       exintro: "",
       explaintext: "",        
       exlimit: 10,
     })
     .success(function(response) {
-      console.log(response);
-      response.query.pages.forEach(function(resp) {
+      var data=response.query.pages;
+      console.log(data);
+      
+      data.forEach(function(resp) {
+        if (resp.thumbnail){
+          $('#searchResult').append(
+            "<a  href='" + resp.fullurl + "' target= '_blank'><div id='result' class='results'><h3>" + resp.title + "</h3><p  class='extract'>" + resp.extract.substring(0,resp.extract.indexOf('.')+1) + "</p> </div> <img src=" + resp.thumbnail.source+  "><hr>");
+          
+        }
+      });
+      data.forEach(function(resp) {
+        
         $('#searchResult').append(
-          "<a href='" + resp.fullurl + "' target= '_blank'><div id='result' class='results'><h3>" + resp.title + "</h3><p  class='extract'>" + resp.extract.substring(0,resp.extract.indexOf('.')+1) + "</p></div>");
-
-      })
+          "<a  href='" + resp.fullurl + "' target= '_blank'><div id='result' class='results'><h3>" + resp.title + "</h3><p  class='extract'>" + resp.extract.substring(0,resp.extract.indexOf('.')+1) + "</p> </div><hr>");
+        
+      }) ;               
+      
 
     });
 
-
-  }); // search
+  }
+  // search
 
 
 
   // trigger submit on use of enter key
   $("#search").keyup(function(event) {
     if (event.keyCode == 13) {
-      $("#searchSubmit").click();
+     click();
 
-    }
-  });
+   }
+ });
 
 
