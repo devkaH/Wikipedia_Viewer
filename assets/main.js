016244e51b580e466b30ac4d0c1f48d2059e9272
kaoula <ben.hammadi.kaoula@gmail.com>
2017-11-15 16:49:44 +0100

    add readme

---
+++ assets/main.js
@@ -0,0 +1,58 @@
+const $ = window.$
+var apiURL = 'https://en.wikipedia.org/w/api.php?callback=?'
+
+function searchResult () {
+// call ajax for get informations
+  $.getJSON(apiURL, {
+    action: 'query',
+    format: 'json',
+    inprop: 'url',
+    formatversion: 2,
+    prop: 'extracts|pageimages|info',
+    generator: 'search',
+    gsrsearch: $('#search').val(),
+    gsrwhat: 'text',
+    piprop: 'thumbnail',
+    pilimit: 'max',
+    pithumbsize: 200,
+    exsentences: 3,
+    exintro: '',
+    explaintext: '',
+    exlimit: 10
+  })
+.success(function (response) {
+  updateUI(response)
+})
+  // Display the search results (article+image)
+  function updateUI (response) {
+    var data = response.query.pages
+    data.forEach(function (resp) {
+      if (resp.thumbnail) {
+        $('#searchResult').append(
+          "<a  href='" + resp.fullurl + "' target= '_blank'><div id='result' class='results'><h3>" + resp.title + "</h3><p  class='extract'>" + resp.extract.substring(0, resp.extract.indexOf('.') + 1) + '</p> </div> <img src=' + resp.thumbnail.source + '><hr>')
+      }
+    })
+    data.forEach(function (resp) {
+      $('#searchResult').append(
+        "<a  href='" + resp.fullurl + "' target= '_blank'><div id='result' class='results'><h3>" + resp.title + "</h3><p  class='extract'>" + resp.extract.substring(0, resp.extract.indexOf('.') + 1) + '</p> </div><hr>')
+    })
+  }
+}
+
+ // clear prior search results
+function clearResults () {
+  $('.searchBox').append("<img id='clearIcon' src='icon.png'>")
+  $('#clearIcon').click(function () {
+    $('#searchResult').empty()
+    $('#search').val('')
+    $('#clearIcon').remove()
+  })
+}
+
+  // trigger submit on use of enter key
+$('#search').keyup(function (event) {
+  if (event.keyCode === 13) {
+    searchResult()
+    clearResults()
+  }
+})
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Wikipedia viewer
+In this mini-project I have developed a  web application that shows search result using wikipidea API, as part of my full stack JavaScript training offered by [GOMYCODE Tunisia](https://www.gomycode.tn)
+## Project brief
+This is a small web application wich does the following:
+
+<strong>User Story</strong>  I can search Wikipedia entries in a search box and see the resulting Wikipedia entries.
+
+<strong>User Story</strong>  I can click a button to see a random Wikipedia entry.
+
+<strong>User Story</strong>  I can click a button to clear search results.
+
+
+
+The following screenshot shows my App:
+<p align="center">
+	<img src="assets/wikiAppImg.png">
+</p>
+
+## What I have learned
+
+* Use wikipedea API.
+
+* Use jquery to manipulate the response of ajax call.
+
+* Integrate  bootstrap template in our project .
